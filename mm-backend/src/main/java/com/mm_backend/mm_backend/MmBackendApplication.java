0ff4f9eea418be28b5891136868cbfef8dac7d9c
Mauricio Lopez <mltbh@Mauricios-MacBook-Pro.local>
2025-09-13 11:48:12 -0700

    First commit

---
+++ mm-backend/src/main/java/com/mm_backend/mm_backend/MmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mm_backend.mm_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmBackendApplication.class, args);
+	}
+
+}
